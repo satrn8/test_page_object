f8bbda433e3646a7a31c3df3491b1422e74d3024
satrn8 <verypyc@gmail.com>
2022-10-25 23:48:11 +0300

    Initial commit

---
+++ tests/test_pages.py
@@ -0,0 +1,79 @@
+from selene import by
+import allure
+
+
+def test_find(setup_browser):
+    browser = setup_browser
+    """
+    Поиск на странице с ошибкой в слове
+    :return:
+    """
+    with allure.step("Поиск на странице"):
+        browser.open("https://www.labirint.ru/")
+        browser.element('[class="b-header-b-search-e-input"]').type("блакнот")
+        browser.element('[class="b-search-e-input-wrapper"]').click()
+
+
+def test_click_office(setup_browser):
+    browser = setup_browser
+    """
+    Выбор офисных принадлежностей на главной странице
+    :param browser_desktop: параметры браузера
+    :return:
+    """
+    browser.open("/")
+    browser.element('[href="/office/"]').click()
+    browser.element('[href="/genres/2300/"]').click()
+
+
+def test_id_books(setup_browser):
+    browser = setup_browser
+    """
+    Добавление товаров по ID в корзину и оформление заказа
+    :param browser_desktop:
+    :return:
+    """
+    browser.open("/")
+    browser.element('[class="b-header-b-search-e-input"]').type("848208")
+    browser.element('[class="b-search-e-input-wrapper"]').click()
+    browser.element('[class="btn buy-link btn-primary"]').click()
+    browser.element('[class="b-header-b-search-e-input"]').type("838271")
+    browser.element('[class="b-search-e-input-wrapper"]').click()
+    browser.element('[class="btn buy-link btn-primary"]').click()
+    browser.element('[class="b-header-b-personal-e-list-item have-dropdown  last-child have-dropdown-notouch"]').click()
+    browser.config.timeout = 20
+    browser.element('[class="btn btn-primary btn-large fright start-checkout-js"]').click()
+
+    first_name = by.xpath('//input[@placeholder="Имя"]')
+    browser.element(first_name).type("Алёна")
+    last_name = by.xpath('//input[@placeholder="Фамилия"]')
+    browser.element(last_name).type("Иванова")
+    user_number = by.xpath('//input[@placeholder="Мобильный телефон"]')
+    browser.element(user_number).type("89998555611")
+    user_email = by.xpath('//input[@placeholder="Электронная почта"]')
+    browser.element(user_email).type("test@test.ru")
+
+
+def test_school(setup_browser):
+    browser = setup_browser
+    browser.open("/")
+    school = by.xpath('//li[@class="b-header-b-menu-e-list-item b-toggle b-header-b-menu-e-list-item-m-temp analytics-click-js"][1]')
+    browser.element(school).click()
+    price_min = by.xpath('//input[@id="section-search-form-price_min"]')
+    browser.element(price_min).type(150)
+    price_max = by.xpath('//input[@id="section-search-form-price_max"]')
+    browser.element(price_max).type(2500)
+    show_book = by.xpath('//input[@class="btn btn-primary btn-small" and @value="Показать"]')
+    browser.element(show_book).click()
+    browser.config.timeout = 100
+
+
+def test_shipping_and_payment(setup_browser):
+    browser = setup_browser
+    browser.open("/")
+    button_shipping_and_payment = ('//li[@class="b-header-b-sec-menu-e-list-item first-child analytics-click-js" and @data-event-content="Доставка и оплата"]')
+    browser.element(button_shipping_and_payment).click()
+    browser.element("#txtwordsadv").type("оплата qr")
+    button_find = by.xpath('//input[@class="btn btn-small btn-primary" and @value="Найти"]')
+    browser.element(button_find).click()
+
